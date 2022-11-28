2477a707480f2f4c521cfaf8ffa2d9da24b68438
merazkevin <merazkevin@yahoo.com>
2022-11-27 21:34:43 -0800

    still working

--- notes/Program.cs
+++ notes/Program.cs
@@ -54,40 +54,52 @@
 // Console.WriteLine($"this are the Array indexes' values [{string.Join(",",newArray)}]");
 // Try to Console.WriteLine your array. What result do you get? Weird, right? Do a bit of research on this to find out why it happens and how to work around it!
 
-// Initializing an empty list of Motorcycle Manufacturers that expects string values
-List<string> bikes = new List<string>();
-// Use the Add function in a similar fashion to push
-bikes.Add("Kawasaki");
-bikes.Add("Triumph");
-bikes.Add("BMW");
-bikes.Add("Moto Guzzi");
-bikes.Add("Harley Davidson");
-bikes.Add("Suzuki");
-// Accessing a generic list value is the same as you would an array
-Console.WriteLine(bikes[2]); //Prints "BMW", remember we start at 0!
-// To get the size of a List, we use Count instead of Length
-Console.WriteLine($"We currently know of {bikes.Count} motorcycle manufacturers.");
+// // Initializing an empty list of Motorcycle Manufacturers that expects string values
+// List<string> bikes = new List<string>();
+// // Use the Add function in a similar fashion to push
+// bikes.Add("Kawasaki");
+// bikes.Add("Triumph");
+// bikes.Add("BMW");
+// bikes.Add("Moto Guzzi");
+// bikes.Add("Harley Davidson");
+// bikes.Add("Suzuki");
+// // Accessing a generic list value is the same as you would an array
+// Console.WriteLine(bikes[2]); //Prints "BMW", remember we start at 0!
+// // To get the size of a List, we use Count instead of Length
+// Console.WriteLine($"We currently know of {bikes.Count} motorcycle manufacturers.");
 
-// Using our list of motorcycle manufacturers from before
-// we can easily loop through the list of them with a for loop
-// this time, however, Count is the attribute for a number of items.
-Console.WriteLine("The current manufacturers we have seen are:");
-for (int idx = 0; idx < bikes.Count; idx++)
-{
-    Console.WriteLine("-" + bikes[idx]);
-}
-// Insert a new item between a specific index
-bikes.Insert(2, "Yamaha");
-// Removal from List
-// Remove is a lot like Javascript array pop, but searches for a specified value
-bikes.Remove("Suzuki");
-bikes.Remove("Yamaha");
-// We can also remove from a specific index
-bikes.RemoveAt(0); 
-// RemoveAt has no return value however, so we cannot get back what we removed
-// The updated list can then be iterated through using a foreach loop
-Console.WriteLine("List of Non-Japanese Manufacturers:");
-foreach (string manu in bikes)
-{
-    Console.WriteLine("-" + manu);
-}
+// // Using our list of motorcycle manufacturers from before
+// // we can easily loop through the list of them with a for loop
+// // this time, however, Count is the attribute for a number of items.
+// Console.WriteLine("The current manufacturers we have seen are:");
+// for (int idx = 0; idx < bikes.Count; idx++)
+// {
+//     Console.WriteLine("-" + bikes[idx]);
+// }
+// // Insert a new item between a specific index
+// bikes.Insert(2, "Yamaha");
+// // Removal from List
+// // Remove is a lot like Javascript array pop, but searches for a specified value
+// bikes.Remove("Suzuki");
+// bikes.Remove("Yamaha");
+// // We can also remove from a specific index
+// bikes.RemoveAt(0); 
+// // RemoveAt has no return value however, so we cannot get back what we removed
+// // The updated list can then be iterated through using a foreach loop
+// Console.WriteLine("List of Non-Japanese Manufacturers:");
+// foreach (string manu in bikes)
+// {
+//     Console.WriteLine("-" + manu);
+// }
+
+// List<int> MyNums = new List<int>();
+// MyNums.Add(2);
+// MyNums.Add(-1);
+// MyNums.Add(7);
+// MyNums.Add(9);
+// MyNums.Add(12);
+
+// for(int i =0; i< MyNums.Count+1;i++)
+// {
+//     Console.WriteLine(MyNums[i]);
+// }
