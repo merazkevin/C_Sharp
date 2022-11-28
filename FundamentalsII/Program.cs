9806e65e2734e4b9128a7a4d4592063a84f03ea3
merazkevin <merazkevin@yahoo.com>
2022-11-27 19:02:50 -0800

    sunday sesh

---
+++ FundamentalsII/Program.cs
@@ -0,0 +1,71 @@
+﻿// Three Basic Arrays
+// Create an array with the integers 0 through 9 inside.
+int[] numsArray = {1,2,3,4,5,6,7,8,9};
+// Create an array with the names "Tim", "Martin", "Nikki", and "Sara".
+string[] namesArray = new string[] {"Tim", "Nikki", "Sara"};
+// Create an array of length 10. Then fill it with alternating true/false values, starting with true. (Tip: do this using logic! Do not hard-code the values in!)
+Random rand = new Random();
+bool[] boolArray = new bool[10];
+int ii =1;
+while(ii<=9){
+    int num = rand.Next(1,3);
+    if(num==1){
+        boolArray[ii]=true;
+        ii++;
+    }
+    else{
+        boolArray[ii]=false;
+        ii++;
+    }
+}
+// Console.WriteLine(String.Join(",",boolArray));
+
+// List of Flavors
+// Create a List of ice cream flavors that holds at least 5 different flavors. (Feel free to add more than 5!)
+List<string> flavors = new List<string>();
+flavors.Add("vanilla");
+flavors.Add("Chocolate");
+flavors.Add("Straberry");
+flavors.Add("BlueBerry");
+flavors.Add("Mint");
+foreach(string flavor in flavors){
+    // Console.WriteLine(flavor);
+}
+
+// Output the length of the List after you added the flavors.
+foreach(string flavor in flavors){
+    // Console.WriteLine(flavor);
+}
+// Console.WriteLine(flavors.Count);
+
+// Output the third flavor in the List.
+// Console.WriteLine(flavors[2]);
+
+// Now remove the third flavor using its index location.
+flavors.Remove(flavors[2]);
+// Output the length of the List again. It should now be one fewer.
+foreach(string flavor in flavors){
+    // Console.WriteLine(flavor);
+}
+// Console.WriteLine(flavors.Count);
+
+
+// User Dictionary
+// Create a dictionary that will store string keys and string values.
+Dictionary<string,string> newDict = new Dictionary<string, string>(); 
+
+// Add key/value pairs to the dictionary where:
+// Each key is a name from your names array.
+// Each value is a randomly selected flavor from your flavors List
+// Loop through the dictionary and print out each user's name and their associated ice cream flavor.
+foreach(string name in namesArray){
+        int RandomIndexNum = rand.Next(0,4);
+        string Randflavor = flavors[RandomIndexNum];
+        newDict.Add(name,Randflavor);
+    };
+
+foreach(KeyValuePair<string,string> key in newDict){
+Console.WriteLine(key);
+}
+
+
--- notes/Program.cs
+++ notes/Program.cs
@@ -1,5 +1,93 @@
-﻿Random rand = new Random();
-for(int i = 1; i <= 10; i++)
+﻿// Random rand = new Random();
+// for(int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine(rand.Next(2,8));
+// }
+
+// Declaring an array of length 5, initialized by default to all zeroes
+// int[] numArray = new int[5];
+// Declaring an array with pre-populated values
+// For arrays initialized this way, the length is determined by the size of the given data
+// int[] numArray2 = {1,2,3,4,6};
+// int ii=0;
+// while(ii<5){
+// Console.WriteLine(numArray2[ii]);
+// ii++;
+// }
+// foreach(int number in numArray2){
+//     Console.WriteLine($"this is the current iteratin {number}");
+// }
+
+// int[] array3; // Declared without initializing a size
+// array3 = new int[] {1,3,5,7,9}; // We can now fill the array by using the new operator
+// array3[1]=0;
+// int ii=0;
+// while(ii<array3.Length){
+// Console.WriteLine(array3[ii]);
+// ii++;
+// }
+
+// int[] arr = {1, 2, 3, 4};
+// Console.WriteLine($"The first number of the arr is {arr[0]}"); 
+// arr[0] = 8;
+// Console.WriteLine($"The first number of the arr is now {arr[0]}");
+// // The array has now changed!
+
+// string[] myCars = new string[] { "Mazda Miata", "Ford Model T", "Dodge Challenger", "Nissan 300zx"}; 
+// foreach (string car in myCars)
+// {
+//     // We no longer need the index, because variable 'car' already represents each indexed value
+//     Console.WriteLine($"I own a {car}");
+// }
+
+// Create an array of integers that will hold 8 values. (Do not fill in the values, just create 8 slots.)
+// int[] newArray= new int[8];
+// Place the number 17 in the third spot of your array. (Think carefully about the index number you need to make this happen!)
+// newArray[2]=17;
+// Try to place the word "Hello" in the fifth spot. What happens?
+// newArray[4]="hello";
+// Loop through your array and print out the values.
+// foreach(int number in newArray){
+//     Console.WriteLine($"this is the Array {number}");
+// }
+// If you did the above action with a for loop, do it again but with a foreach loop.
+// Console.WriteLine($"this are the Array indexes' values [{string.Join(",",newArray)}]");
+// Try to Console.WriteLine your array. What result do you get? Weird, right? Do a bit of research on this to find out why it happens and how to work around it!
+
+// Initializing an empty list of Motorcycle Manufacturers that expects string values
+List<string> bikes = new List<string>();
+// Use the Add function in a similar fashion to push
+bikes.Add("Kawasaki");
+bikes.Add("Triumph");
+bikes.Add("BMW");
+bikes.Add("Moto Guzzi");
+bikes.Add("Harley Davidson");
+bikes.Add("Suzuki");
+// Accessing a generic list value is the same as you would an array
+Console.WriteLine(bikes[2]); //Prints "BMW", remember we start at 0!
+// To get the size of a List, we use Count instead of Length
+Console.WriteLine($"We currently know of {bikes.Count} motorcycle manufacturers.");
+
+// Using our list of motorcycle manufacturers from before
+// we can easily loop through the list of them with a for loop
+// this time, however, Count is the attribute for a number of items.
+Console.WriteLine("The current manufacturers we have seen are:");
+for (int idx = 0; idx < bikes.Count; idx++)
 {
-    Console.WriteLine(rand.Next(2,8));
+    Console.WriteLine("-" + bikes[idx]);
+}
+// Insert a new item between a specific index
+bikes.Insert(2, "Yamaha");
+// Removal from List
+// Remove is a lot like Javascript array pop, but searches for a specified value
+bikes.Remove("Suzuki");
+bikes.Remove("Yamaha");
+// We can also remove from a specific index
+bikes.RemoveAt(0); 
+// RemoveAt has no return value however, so we cannot get back what we removed
+// The updated list can then be iterated through using a foreach loop
+Console.WriteLine("List of Non-Japanese Manufacturers:");
+foreach (string manu in bikes)
+{
+    Console.WriteLine("-" + manu);
 }
