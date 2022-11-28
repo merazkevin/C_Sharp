c0cb50d9eb93735430ded81171ce8a97ceca2d5e
merazkevin <merazkevin@yahoo.com>
2022-11-27 21:34:27 -0800

    still working

---
+++ DebuggingPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿// Challenge 1
+//bool amProgrammer = "true"; //<=== wrong data type ===>
+// <=== Correction ===> 
+string amProgrammer = "true";
+
+//int Age = 27.9; //<=== wrong data type ===>
+// <=== Correction ===> 
+Double Age = 27.9;
+
+List<string> Names = new List<string>();
+Names.Add = "Monica";
+Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
+MyDictionary.Add("Hello", "0");
+//MyDictionary.Add("Hi there", 0);//<=== wrong data type ===>
+// <=== Correction ===> 
+MyDictionary.Add("Hi there", "0");// missing -> (""). 
+// This is a tricky one! Hint: look up what a char is in C#
+//string MyName = 'MyName'; // missing -> ("").
+// <=== Correction ===> 
+string MyName = "MyName";
+
+Console.WriteLine(amProgrammer);
+Console.WriteLine(Age);
+Console.WriteLine(MyName);
