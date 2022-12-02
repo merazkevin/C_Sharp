5d489b42ce4acf4864b3d43dd9a6ed3e74abc854
merazkevin <merazkevin@yahoo.com>
2022-12-01 22:46:37 -0800

    portfolio 2

--- PorfolioI/Controllers/IndexController.cs
+++ PorfolioI/Controllers/IndexController.cs
@@ -6,8 +6,10 @@ public class IndexController : Controller   // Remember inheritance?
 {      
     [HttpGet("")] // We will go over this in more detail on the next page    
 
-    public ViewResult Index()        
+    public IActionResult Index()        
     {            
+        ViewBag.kevin = "kevin";
+        ViewBag.Example = "Hellooo World!";
         return View ("Index");        
     } 
     
