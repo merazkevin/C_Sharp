e9554caee9dd7fc01bcd633d3bb6f7bf7c50904a
merazkevin <merazkevin@yahoo.com>
2022-12-03 13:55:11 -0800

    dojoSurveyWIth Model

---
+++ DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    // <=== Constructor ===>
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    // <=== Get Routes ===>
+    //  <== index ==> 
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    //  <== privacy ==>
+    [HttpGet("Privacy")]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    //  <== DojoForm ==>
+    [HttpGet("DojoForm")]
+    public IActionResult DojoForm()
+    {
+        return View();
+    }
+
+    //<=== Post Routes ===>
+    //  <== DojoForm ==>
+    [HttpPost("ProcessDojoForm")]
+    public IActionResult ProcessDojoForm(DojoFormClass oneForm)
+    {
+        return View(oneForm);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ DojoSurveyWithModel/Models/DojoFormModel.cs
@@ -0,0 +1,9 @@
+namespace DojoSurveyWithModel;
+
+public class DojoFormClass
+{
+    public string? name {get;set;}
+    public string? dojoLocation {get;set;}
+    public string? favoriteLanguage {get;set;}
+    public string? comment {get;set;}
+} 
